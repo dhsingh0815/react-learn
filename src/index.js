399c7b42fb6bc36b97174a956e1fc1b81cd42ce2
dhsingh0815 <32283056+dhsingh0815@users.noreply.github.com>
2024-12-10 11:39:25 +0000

    Interactive app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./DesignThinkingInReact";
+//import App from "./DesignThinkingInReact";
 //import App from "./DisplayableReactComponent";
 //import App from "./ReactComponentWithStateAndEventHandler";
-
+import App from "./DesignThinkingInReactInteractive";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
